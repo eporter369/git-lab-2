665b27bd87d020ee82b5edee42584b1cb419507f
eporter369 <eporter2306@gmail.com>
2025-01-22 17:21:01 -0500

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/**
+ * @file: git-lab-program.cc
+ * @author: Evan Porter
+ * @date: 01/22/2025
+ * @brief: A program created for the purpose of understanding git and GitHub.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+} // main
